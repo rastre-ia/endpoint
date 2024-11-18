748b4e9e30c415ab95a47defe53c08acfab818bf
Reinaldo-Kn <reinaldon@alunos.utfpr.edu.br>
2024-11-18 16:04:52 -0300

    added license

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.routes import info, image_embeddings, text_embeddings, vector_search, chat, llama_search, llama_search_2
+from app.routes import info, image_embeddings, text_embeddings, vector_search, chat, llama_search
 import os
+
 
 uri = os.getenv("MONGODB_URI")
 
@@ -22,7 +23,6 @@ app.include_router(chat.router, prefix="/chat", tags=["Chat"])
 
 app.include_router(llama_search.router, prefix="/llama-search", tags=["Llama Search"])
 
-app.include_router(llama_search_2.router, prefix="/llama-search-2", tags=["Llama Search 2"])
 @app.get("/")
 def root():
     return {"message": "Backend is running"}
