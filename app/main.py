f7c8379a184b92dc7c24724159c5ee8554920b8e
Reinaldo-Kn <reinaldon@alunos.utfpr.edu.br>
2025-03-05 18:10:43 -0300

    CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import ai_search, info, image_embeddings, text_embeddings, vector_search, chat
 import os
 
-
 app = FastAPI()
 
+# Adicionando a middleware CORS
+origins = [
+    "https://rastre-ia-reinaldo-kn-rastreia.vercel.app",  # Seu domínio Vercel
+    "*",  # Para permitir outras origens (remova "*" quando for para produção)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],  
+)
+
+# Incluindo as rotas da aplicação
 app.include_router(info.router, prefix="/info", tags=["Info"])
 app.include_router(
     image_embeddings.router, prefix="/image-embeddings", tags=["Image Embeddings"]
@@ -15,9 +30,7 @@ app.include_router(
 app.include_router(
     vector_search.router, prefix="/vector-search", tags=["Vector Search"]
 )
-
 app.include_router(chat.router, prefix="/chat", tags=["Chat"])
-
 app.include_router(ai_search.router, prefix="/ai-search", tags=["Llama Search"])
 
 @app.get("/")
