3a27cfde01ea280dd37bafb9fab2b055cbb29873
Reinaldo-Kn <reinaldon@alunos.utfpr.edu.br>
2025-03-04 22:00:24 -0300

    remove some dependencies

--- app/main.py
+++ app/main.py
@@ -3,9 +3,6 @@ from app.routes import ai_search, info, image_embeddings, text_embeddings, vecto
 import os
 
 
-uri = os.getenv("MONGODB_URI")
-
-
 app = FastAPI()
 
 app.include_router(info.router, prefix="/info", tags=["Info"])
@@ -15,9 +12,9 @@ app.include_router(
 app.include_router(
     text_embeddings.router, prefix="/text-embeddings", tags=["Text Embeddings"]
 )
-# app.include_router(
-#     vector_search.router, prefix="/vector-search", tags=["Vector Search"]
-# )
+app.include_router(
+    vector_search.router, prefix="/vector-search", tags=["Vector Search"]
+)
 
 app.include_router(chat.router, prefix="/chat", tags=["Chat"])
 
