3430cfb1becaeffbd9028bd7a4431eec62c7e504
Reinaldo-Kn <reinaldon@alunos.utfpr.edu.br>
2025-03-16 13:29:47 -0300

    added randon User-Agent to webscrap

--- app/routes/websracp.py
+++ app/routes/websracp.py
@@ -6,12 +6,15 @@ import re
 from langchain_google_genai import GoogleGenerativeAI
 import dotenv
 import os
+from fake_useragent import UserAgent  
 
 dotenv.load_dotenv()
 
 router = APIRouter()
 google_api_key = os.getenv("GOOGLE_API_KEY")
 llm_model = os.getenv("GOOGLE_MODEL")
+
+ua = UserAgent()  
 
 class Query(BaseModel):
     search_term: str
@@ -29,10 +32,16 @@ def parse_gemini_response(response: str) -> list:
 async def parse_listings(query: Query):
     
     jina_url = f"https://r.jina.ai/https://www.olx.com.br/brasil?q={query.search_term}&sf=1"
-    
+
+    headers = {
+        "User-Agent": ua.random,  
+        "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Referer": "https://www.olx.com.br/",
+        "Connection": "keep-alive"
+    }
+
     try:
-    
-        response = requests.get(jina_url, timeout=30)
+        response = requests.get(jina_url, headers=headers, timeout=60)
         response.raise_for_status()
         markdown_content = response.text[:9000]  
     except requests.exceptions.RequestException as e:
@@ -56,14 +65,12 @@ async def parse_listings(query: Query):
         
         parsed_data = parse_gemini_response(result)
         
-
         if not isinstance(parsed_data, list):
             raise ValueError("Formato inválido na resposta")
             
         for item in parsed_data:
-            if not all(key in item for key in ['title', 'location', 'img_url']):
+            if not all(key in item for key in ['title', 'location', 'img_url','product_url']):
                 raise ValueError("Itens incompletos na resposta")
-
         return {"results": parsed_data}
 
     except Exception as e:
